723c12908f6b730a8442ef4c663b7faffc58b331
kazurayam <kazuaki.urayama@gmail.com>
2021-10-13 18:06:50 +0900

    Listing11-6

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -8,5 +20,20 @@ mod tests {
     #[test]
     fn another() {
         panic!("Make this test fail");
+    }
+
+    use super::*;
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+        assert!(larger.can_hold(&smaller));
     }
 }
